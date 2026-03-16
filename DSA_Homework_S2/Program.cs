cd7d07f2b3f9881848f274c2725b012e7fa7c683
iNuss99 <2500114713@vanlangsaigon.edu.vn>
2026-03-16 10:25:29 +0700

    Hoan Thanh Homework_S2

---
+++ DSA_Homework_S2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+   
+    static int TinhTong(int n)
+    {
+        
+        if (n == 1)
+            return 1;
+
+        return n + TinhTong(n - 1);
+    }
+
+    static long GiaiThuaVongLap(int n)
+    {
+        long ketQua = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            ketQua *= i;
+        }
+
+        return ketQua;
+    }
+
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.Write("Nhập n để tính: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Tổng S(n) = " + TinhTong(n));
+        Console.WriteLine("Giai thừa của n = " + GiaiThuaVongLap(n));
+    }
+}
